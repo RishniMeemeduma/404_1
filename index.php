57a3d18d7aedf402355531a5320daa26b7827697
Rishni <ashameemeduma@gmail.com>
2019-05-15 19:28:26 +0530

    tenth

--- index.php
+++ index.php
@@ -39,3 +39,44 @@
   <!-- End including js -->
 
 </html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Not Found</title>
+	<meta charset="UTF-8">
+  <meta name="description" content="Robot 404 Page">
+  <meta name="keywords" content="HTML,CSS,404,Not found">
+  <meta name="author" content="Rishni Meemeduma">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Include css -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <!-- End including css -->
+
+
+
+</head>
+<body>
+
+	<div class="text">
+		<h1>Opps! Our Bad</h1>
+	</div>
+
+	<!-- Animation -->
+	  <div class="wrapper"></div>
+	<!-- Animation End -->
+
+
+	  <div class="paragraph">
+	  	<p>Your page is not found. Return to <a href="index.html">Home Page</a></p>
+	  </div>
+
+
+</body>
+
+  <!-- Include js -->
+   	<script src="js/anime.min.js"></script>
+ 	<script type="text/javascript" src="js/script.js"></script>
+  <!-- End including js -->
+
+</html>
